d56c92a0edc562e02f485228290fcd7f81587d1b
Neous1 <Neous1@users.noreply.github.com>
2017-09-08 07:18:04 -0400

    add hello.js

--- hello.js
+++ hello.js
@@ -1,9 +1,18 @@
 var http = require("http");
 
+// http.createServer(function(req, res){
+//     res.writeHead(200);
+//     // res.write("Hello, this is dog.");
+//     res.end("Hello, this is dog.");
+// }).listen(8080, function(){
+//     console.log("listening on port 8080...");
+// });
 http.createServer(function(req, res){
-    res.writeHead(200);
+    res.writeHead(200, {"Content-Type: html/text"});
+    fs.readFileSync("index.html", function(){
+        console.log(contents);
+        
+    });
     res.write("Hello, this is dog.");
     res.end();
-}).listen(8080, function(){
-    console.log("listening on port 8080...");
-});
+}).listen(8080);
